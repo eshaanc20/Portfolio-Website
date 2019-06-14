d069310c9bb129f49662a182915b4df99f118d25
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-06-14 14:48:17 -0400

    Website redesigned

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Component} from 'react';
 import Homepage from './Components/Homepage/Homepage.js';
 
-
 class App extends Component{
   render() {
     return (
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,29 +1,8 @@
-import React from 'react';
-import NameCard from '../NameCard/NameCard.js';
-import InfoCard from './InfoCard/InfoCard.js';
-import SkillsCard from './SkillsCard/SkillsCard.js';
-import Contact from './Contact/Contact.js';
-import Connect from './Connect/Connect.js';
+import React from 'react'
 
 const homepage = () => {
-    const style = {
-        display: 'flex',
-        flexWrap: 'wrap', 
-        justifyContent: 'space-evenly',
-        width: '98%',
-        paddingBottom: '14px',
-        paddingTop: '20px',
-        marginLeft: '16px',
-    }
     return(
         <div>
-            <NameCard/>
-            <div style = {style}>
-                <InfoCard/>
-                <Contact/>
-                <SkillsCard/>
-                <Connect/>
-            </div>
         </div>
     );
 }
