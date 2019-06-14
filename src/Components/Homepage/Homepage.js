92175dd715d6c8b1ef90db6c6a7a20619089e018
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-06-14 17:01:27 -0400

    Created new homepage

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Homepage.css';
+import ProfilePicture from '../../Assets/profilePicture.jpg';
 
 const homepage = () => {
     return(
-        <div>
+        <div className = 'homepage'>
+            <img className='profile' src={ProfilePicture}/>
+            <p className='name'>Eshaan Chaudhari</p>
+            <p>Software Engineering and Management Student</p>
+            <p>McMaster University</p>
         </div>
     );
 }
