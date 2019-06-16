aab5a6fdecd26397e439167ee7bd17b173ac5684
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-06-15 23:02:57 -0400

    CSS updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Component} from 'react';
 import Homepage from './Components/Homepage/Homepage.js';
 import Navigation from './Components/Navigation/Navigation.js';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom';
 
 class App extends Component{
   render() {
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -6,13 +6,13 @@ const navigation = (props) => {
         <div className = 'navigation'>
             <p className='navigationName'>Eshaan</p>
             <div className = 'Nav'>
-                <a href='/contact'>Contact</a>
-                <a href='/online-courses'>Online courses</a>
-                <a href='/projects'>Projects</a>
-                <a href='/skills'>Skills</a>
-                <a href='/experience'>Experience</a>
+                <a href='/'>Home</a>
                 <a href='/education'>Education</a>
-                <a className='homeLink' href='/'>Home</a>
+                <a href='/experience'>Experience</a>
+                <a href='/skills'>Skills</a>
+                <a href='/projects'>Projects</a>
+                <a href='/online-courses'>Online courses</a>
+                <a href='/contact'>Contact</a>
             </div>
         </div>
     );
