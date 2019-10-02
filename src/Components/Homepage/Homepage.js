71c27a7c5a69e0f7a02be7e43dc49a75a9799558
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-10-02 00:02:07 -0400

    Edited CSS

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Homepage.css';
 import ProfilePicture from '../../Assets/profilePicture.jpg';
 import {IoLogoLinkedin, IoLogoGithub} from 'react-icons/io';
+import Typing from 'react-typing-animation';
 import Button from '@material-ui/core/Button';
 import Resume from '../../Eshaan_Chaudhari_Resume.pdf';
 
@@ -9,34 +10,36 @@ const homepage = () => {
     return(
         <div className = 'homepage'>
             <img className='profile' alt='' src={ProfilePicture}/>
-            <p className='name'>Eshaan Chaudhari</p>
+            <Typing speed={100}>
+                <p className='name'>Eshaan Chaudhari</p>
+            </Typing>
             <p className='p1'>Software Engineering and Management Student</p>
             <p className='p1'>McMaster University</p>
-            <a className='connect'>
+            <a className='connect' href = 'https://linkedin.com/in/eshaan-chaudhari'>
                 <IoLogoLinkedin/>
             </a>
-            <a className='connect'>
+            <a className='connect' href = 'https://github.com/eshaanc20'>
                 <IoLogoGithub className='connect'/>
             </a>
             <div>
-                <Button 
-                    variant='contained' 
-                    style={{
-                        marginRight:'20px', 
-                        backgroundColor:'darkgreen',
-                        color: 'white'
-                    }}
-                ><a href='/contact' style={{textDecoration:'none', color:'white'}}>Contact</a>
-                </Button>
-                <Button 
+            <Button 
                     variant='contained'
                     style={{
                         backgroundColor:'darkcyan',
+                        marginRight:'20px',
                         color: 'white'
                     }}
                     href = {Resume}
                     target = '_blank'
                 >View Resume</Button>
+                <Button 
+                    variant='contained' 
+                    style={{
+                        backgroundColor:'darkgreen',
+                        color: 'white'
+                    }}
+                ><a href='/contact' style={{textDecoration:'none', color:'white'}}>Contact</a>
+                </Button>
             </div>
         </div>
     );
